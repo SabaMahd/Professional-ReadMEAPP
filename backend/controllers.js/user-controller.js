a2c4359679795d9f3bca5f6210081ac8dda39f33
ChotanSharma <chotansharma@yahoo.com>
2022-05-01 12:42:10 -0400

    add CRUD func for User model

--- backend/controllers.js/user-controller.js
+++ backend/controllers.js/user-controller.js
@@ -1,11 +1,10 @@
 const { User } = require('../models');
 
 const userController = {
-  // the functions will go in here as methods
     // get all users
     getAllPizza(req, res) {
       User.find({})
-        .then(dbUserData => res.json(dbuserData))
+        .then(dbUserData => res.json(dbUserData))
         .catch(err => {
           console.log(err);
           res.status(400).json(err);
@@ -28,6 +27,39 @@ const userController = {
           res.status(400).json(err);
         });
     },
+
+    // create users
+    createUser({ body }, res) {
+      User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // update user by id
+    updateUser({ params, body }, res) {
+      User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
   
 };
 
