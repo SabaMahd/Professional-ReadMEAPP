e54b4b87063d4162369463b4597441d0dd86510e
ChotanSharma <chotansharma@yahoo.com>
2022-05-02 08:50:32 -0400

    user routes working

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 // Import all of the API routes from /api/index.js 
 const apiRoutes = require('./api');
-const htmlRoutes = require('./html/html-routes');
+// waiting for the front-end codes to hook up
+//const htmlRoutes = require('./html/html-routes');
 
 // add prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
+
+//waiting for the front end here
+//router.use('/', htmlRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.static('public'));
 
-//app.use(require('./routes'));
+app.use(require('./routes'));
 app.use('*', (req,res) => res.status(404).json({error: 'Not Found'}))
 
 
