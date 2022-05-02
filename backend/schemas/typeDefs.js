1f816ea97f3cff561669f0f045fb6b7fe496bfb4
ChotanSharma <chotansharma@yahoo.com>
2022-05-02 08:51:49 -0400

    ready to push into develop

--- backend/schemas/typeDefs.js
+++ backend/schemas/typeDefs.js
@@ -10,27 +10,6 @@ const typeDefs = gql`
         email: String
     }
 
-    type Query {
-        me: User
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
-    }
-
-    type Auth {
-        token: ID!
-        user: User
-    }
-
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        addThought(thoughtText: String!): Thought
-        addReaction(thoughtId: ID!, reactionBody: String!): Thought
-        addFriend(friendId: ID!): User
-    }
-
 `;
 
 
