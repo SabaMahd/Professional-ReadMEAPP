24d33e09c98ef95ad9964f34d22d3f65abd2576d
ChotanSharma <chotansharma@yahoo.com>
2022-05-02 11:07:58 -0400

    add the dateFormat

---
+++ backend/Models/Readme.js
@@ -0,0 +1,65 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String,
+      required: true
+    },
+    writtenBy: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  commentBody: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
+  replies: [ReplySchema]
+},
+{
+  toJSON: {
+    getters: true,
+    virtuals: true
+  },
+  id: false
+}
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
+
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
