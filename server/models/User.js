5764de5e979516bad3020fa2e8f7f162d6bf81d8
omcewan <orlandomcewan231@gmail.com>
2022-05-02 16:44:18 -0400

    set up user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,55 @@
 const { Schema, model } = require('mongoose');
-const validateEmail = require('../utils/validateEmail')
+const validateEmail = require('../utils/validateEmail');
 const bcrypt = require('bcrypt');
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, 'Please enter a valid email'],
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    files: [{ type: Schema.Types.ObjectId, ref: 'ReadMe' }],
   },
-  
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: [validateEmail, 'Please enter a valid email']
+  {
+    toJSON: {
+      virtuals: true,
+    },
   }
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
 });
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('fileCount').get(function () {
+  return this.files.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
