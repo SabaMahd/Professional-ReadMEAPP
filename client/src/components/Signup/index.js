5c57f53c7fe4c55fa016f427f61a49d4c1226714
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-05-02 15:24:46 -0500

    set up validateEmail helper in Signup component

---
+++ client/src/components/Signup/index.js
@@ -0,0 +1,5 @@
+// Sign-up form component
+import React from "react";
+// Checks to make sure user's email address and password are both unique
+// Validate email address using the RegEx in the helpers.js file in the utils folder
+import { validateEmail } from "../../utils/helpers";
