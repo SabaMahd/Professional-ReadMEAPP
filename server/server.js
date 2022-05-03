b2ddc66a28fde40a594c56fdd5384404bad9bad3
omcewan <orlandomcewan231@gmail.com>
2022-05-02 20:46:12 -0400

    update server.js introducting authmiddleware

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const path = require('path');
 // import apollo server
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
