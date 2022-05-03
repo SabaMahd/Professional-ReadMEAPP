571f7b5a50b2927dfe93496d84ced207055a4fdf
omcewan <orlandomcewan231@gmail.com>
2022-05-02 21:23:00 -0400

    add login logic to resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,12 @@
 const { User, ReadMe } = require('../models');
+const { signToken } = require('../utils/auth');
 
 // define resolvers
 const resolvers = {
   Query: {
     // get all users
     users: async () => {
-      return User.find({})
+      return User.find({});
     },
   },
 
@@ -13,9 +14,28 @@ const resolvers = {
     // add a user
     addUser: async (parent, args) => {
       const user = await User.create(args);
-      return user
-    }
-  }
+      const token = signToken(user);
+      return { user, token };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,7 +30,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }
   
 `;
