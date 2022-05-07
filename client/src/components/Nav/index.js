a9da5f80ca58a09183b1876a7a6cd85e1ae636b3
Harry <chairmanharryogletree@gmail.com>
2022-05-07 15:53:11 -0400

    <clean up code and added navigation>

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,33 +8,24 @@ const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1>Professional-Readme</h1>
+        <h1><a href="/">Professional-Readme</a></h1>
       </div>
       <nav>
         <ul className="flex-row">
           <li className="mx-1">
-            <a href="#about">
-              About me
-            </a>
-          </li>
-          <li className="mx-1">
-            <a href="#ReadmeGenerator">
+            <a href="client\src\components\GenerateReadmeForm">
               ReadmeGenerator
             </a>
           </li>
           <li className="mx-1">
             <a href="#signin">
-              Sign in
-            </a>
-          </li>
-          <li className="mx-1">
-            <a href="#signup">
-              Sign Up
+              Login/Sign Up
             </a>
           </li>
         </ul>
       </nav>
     </header>
+
   );
 };
 
