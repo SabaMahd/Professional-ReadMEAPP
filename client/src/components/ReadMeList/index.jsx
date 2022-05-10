47ebb55fb806f452fea24266cafc8fd57e688aa7
omcewan <orlandomcewan231@gmail.com>
2022-05-10 15:59:46 -0400

    set up ReadList layout

--- client/src/components/ReadMeList/index.jsx
+++ client/src/components/ReadMeList/index.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import { GET_ME, COMPOSE_README } from '../../utils/queries';
 import { DELETE_READ_ME } from '../../utils/mutations';
 import { useMutation, useQuery } from '@apollo/client';
 import Auth from '../../utils/auth';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function ReadMeList() {
-  const { loadingUserData, Userdata } = useQuery(GET_ME);
+  const arr = [1, 2, 3, 4, 5];
+  const { loadingUserData, userData } = useQuery(GET_ME);
   // const { loadingReadMeData, readmeData } = useQuery(COMPOSE_README);
   const [deleteReadMe] = useMutation(DELETE_READ_ME);
 
+  if (userData) {
+    console.log(userData.me.files);
+  }
+
+  // return (
+  //   <ButtonGroup className="d-grid gap-2">
+  //     <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-nested-dropdown">
+  //       <Dropdown.Item eventKey="1">Genereate README</Dropdown.Item>
+  //       <Dropdown.Item eventKey="2">Delete README</Dropdown.Item>
+  //       <Dropdown.Item eventKey="3">Download</Dropdown.Item>
+  //     </DropdownButton>
+  //   </ButtonGroup>
+  // );
   return (
-    <ButtonGroup className="d-grid gap-2" >
-      <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-nested-dropdown" >
-        <Dropdown.Item eventKey="1" >Genereate README</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Delete README</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Download</Dropdown.Item>
-      </DropdownButton>
-    </ButtonGroup>
+    <Row>
+      {arr.map((element) => (
+        <Col lg={4} key={element}>
+          <ButtonGroup>
+            <DropdownButton
+              as={ButtonGroup}
+              title="Dropdown"
+              id="bg-nested-dropdown"
+            >
+              <Dropdown.Item eventKey="1">Genereate README</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Delete README</Dropdown.Item>
+              <Dropdown.Item eventKey="3">Download</Dropdown.Item>
+            </DropdownButton>
+          </ButtonGroup>
+        </Col>
+      ))}
+    </Row>
   );
 }
 
