0b77a33e16b7de0e1bdab9b582bf922eb6329887
Josh <biscuitemail2@gmail.com>
2022-05-11 02:40:44 -0400

    media querey for nav bar responce

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import Auth from '../utils/auth';
 import { Link } from 'react-router-dom';
-import NavTabs from '../NavTabs';
+// import NavTabs from '../NavTabs';
 //import SigninForm from '../src/components/Signup';
 //import SigninForm from '../src/components/Signin';
 
@@ -10,35 +10,42 @@ function Header({
 }) {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center justify-space-between-lg">
+  
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>Professional-Readme</h1>
         </Link>
         {/* <h1><a href="/">Professional-Readme</a></h1> */}
       </div>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/home">README Generator</Link>
+      <nav class>
+
+ 
+
+   
+     <div> 
+        <ul class="flex-row navbar-nav">
+          <li class="mx-1 nav-item">
+            <Link  className="nav-link" to="/home">README Generator</Link>
             {/* <a href="client\src\components\GenerateReadmeForm">
               ReadmeGenerator
             </a> */}
           </li>
-          <li className="mx-1">
-            <Link to="/login">Login</Link>
+          <li class="mx-1 nav-item">
+            <Link class="nav-link"to="/login">Login</Link>
             {/* <a href="#signin">
               Login/Sign Up
             </a> */}
           </li>
-          <li className="mx-1">
-            <Link to="/signup">Sign-Up</Link>
+          <li class="mx-1 nav-item">
+            <Link class="nav-link" to="/signup">Sign-Up</Link>
             {/* <a href="#signin">
               Login/Sign Up
             </a> */}
           </li>
         </ul>
+        </div>
       </nav>
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+       {/* <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />  */}
     </header>
   );
 };
